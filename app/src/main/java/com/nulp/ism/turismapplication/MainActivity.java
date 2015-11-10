5392b02db52026fcd4838b07cad2eb15d286acce
nero15 <vasyl.dmytriv94@gmail.com>
2015-11-10 23:26:50 +0200

    Підключив Google Maps. Виводить мітки по заданим координатам.

--- app/src/main/java/com/nulp/ism/turismapplication/MainActivity.java
+++ app/src/main/java/com/nulp/ism/turismapplication/MainActivity.java
@@ -1,16 +1,35 @@
 package com.nulp.ism.turismapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.location.places.personalized.HereContent;
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity {
+
+    GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createMapView();
+        addMarker(0, 0, "Marker 1");
+        addMarker(40, 34, "Marker 2");
+        addMarker(28, 40, "Marker 3");
+        addMarker(64, 33, "Marker 4");
+        addMarker(-14, 36, "Marker 5");
     }
 
     @Override
@@ -33,5 +52,41 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void createMapView(){
+        /**
+         * Catch the null pointer exception that
+         * may be thrown when initialising the map
+         */
+        try {
+            if(null == googleMap){
+                googleMap = ((MapFragment) getFragmentManager().findFragmentById(
+                        R.id.mapView)).getMap();
+
+                /**
+                 * If the map is still null after attempted initialisation,
+                 * show an error to the user
+                 */
+                if(null == googleMap) {
+                    Toast.makeText(getApplicationContext(),
+                            "Error creating map", Toast.LENGTH_SHORT).show();
+                }
+            }
+        } catch (NullPointerException exception){
+            Log.e("mapApp", exception.toString());
+        }
+    }
+
+    private void addMarker(double x, double y, String name){
+
+        /** Make sure that the map has been initialised **/
+        if(null != googleMap){
+            googleMap.addMarker(new MarkerOptions()
+                            .position(new LatLng(x, y))
+                            .title(name)
+                            .draggable(true)
+            );
+        }
     }
 }
